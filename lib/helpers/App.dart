56a5ead48b843d7e2b7160acc97f0faa31ca54b2
Phillip Karamagi <pkaramagi1@gmail.com>
2021-07-05 19:54:22 +0300

    created app helpers

---
+++ lib/helpers/App.dart
@@ -0,0 +1,8 @@
+// App config
+class HdkApp{
+  static final String dbName = 'hdk_v1.db';
+  static final String apiLink = 'http://172.16.209.155:8000/api/categories/';
+  static final String categoryTableName = 'hdk_category';
+  static final String tpwordsTableName = 'hdk_tpwords';
+
+}
